36f669785e4e1908111fa146297ed7f63fbb80a1
mrazomej <manuel.razo.m@gmail.com>
2019-05-10 12:57:27 -0700

    starting to shape the repo

---
+++ evo_utils/viz.py
@@ -0,0 +1,51 @@
+# Import plotting utilities
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib as mpl
+
+# Seaborn, useful for graphics
+import seaborn as sns
+
+import numpy as np
+
+"""
+Title:
+    viz.py
+Last update:
+    2019-05-10
+Author(s):
+    Manuel Razo-Mejia
+Purpose:
+    This file compiles all of the relevant functions for plotting style
+    related to the evo_mcw project.
+"""
+
+# Default RP plotting style
+def set_plotting_style():
+    """
+    Formats plotting enviroment to that used in Physical Biology of the Cell,
+    2nd edition. To format all plots within a script, simply execute
+    `mwc_induction_utils.set_plotting_style() in the preamble.
+    """
+    rc = {'lines.linewidth': 1.25,
+          'axes.labelsize': 8,
+          'axes.titlesize': 9,
+          'axes.facecolor': '#E3DCD0',
+          'xtick.labelsize': 7,
+          'ytick.labelsize': 7,
+          'font.family': 'Lucida Sans Unicode',
+          'grid.linestyle': '-',
+          'grid.linewidth': 0.5,
+          'grid.color': '#ffffff',
+          'legend.fontsize': 8}
+    plt.rc('text.latex', preamble=r'\usepackage{sfmath}')
+    plt.rc('xtick.major', pad=-1)
+    plt.rc('ytick.major', pad=-1)
+    plt.rc('mathtext', fontset='stixsans', sf='sansserif')
+    plt.rc('figure', figsize=[3.5, 2.5])
+    plt.rc('svg', fonttype='none')
+    plt.rc('legend', title_fontsize='8', frameon=True, 
+           facecolor='#E3DCD0', framealpha=1)
+    sns.set_style('darkgrid', rc=rc)
+    sns.set_palette("colorblind", color_codes=True)
+    sns.set_context('notebook', rc=rc)
