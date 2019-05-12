44e3bc4ff342bf95ef21d46c607756af03fb9702
mrazomej <manuel.razo.m@gmail.com>
2019-05-12 16:24:56 -0700

    adding packages from Griffin's MWC growth

---
+++ evo_utils/__ini__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top level package for evo_utils utilities"""
+
+
+# from . import mcmc
+from . import viz
+from . import fitderiv
+
+
+__author__ = """Manuel Razo"""
+__email__ = """mrazomej {at} caltech.edu"""
+__version__ = '0.0.1'
