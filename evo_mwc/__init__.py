e7f3094614bdae140a7b987667dca3b9a23f4b73
mrazomej <manuel.razo.m@gmail.com>
2019-08-12 13:37:40 -0700

    update package name

--- evo_mwc/__init__.py
+++ evo_mwc/__init__.py
@@ -2,8 +2,6 @@
 
 """Top level package for evo_utils utilities"""
 
-
-# from . import mcmc
 from . import viz
 from . import fitderiv
 from . import model
@@ -12,3 +10,4 @@ from . import model
 __author__ = """Manuel Razo"""
 __email__ = """mrazomej {at} caltech.edu"""
 __version__ = '0.0.1'
+name = 'evo_mwc'
